f9929b4b5ff28f1ddae2c131248c03f8947efa68
Jinal.Shah <28205508+JinalShah2002@users.noreply.github.com>
2024-07-24 19:31:46 -0400

    Web UI is mainly done

---
+++ src/app.py
@@ -0,0 +1,25 @@
+"""
+
+@author: Jinal Shah
+
+This file is the main web UI
+
+"""
+import streamlit as st
+from langchain_openai import OpenAI
+
+st.title('Courtside Analytics')
+
+openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
+
+def generate_response(input_text):
+    llm = OpenAI(openai_api_key=openai_api_key)
+    st.info(llm(input_text))
+
+with st.form('my_form'):
+    text = st.text_area('Enter text:', 'What are the three key pieces of advice for learning how to code?')
+    submitted = st.form_submit_button('Submit')
+    if not openai_api_key.startswith('sk-'):
+        st.warning('Please enter your OpenAI API key!', icon='⚠')
+    if submitted and openai_api_key.startswith('sk-'):
+        generate_response(text)
